86e70d331ddbeefb1e2e34e97e57e05c02f70611
Sciverag <samuelciga2004@gmail.com>
2025-02-05 20:37:18 +0100

    Iniciado el proyecto

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\Api\CarroController;
+use App\Http\Controllers\Api\LoginController;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::delete('carros', [CarroController::class, 'destroy'])->middleware('auth:api');
+Route::put('carros/{idUsuario}/{idProducto}/{cantidad}', [CarroController::class, 'update'])->middleware('auth:api');
+
+Route::apiResource('carros', CarroController::class)->except(['destroy','update']);
+
+Route::post('login', [LoginController::class, 'login']);
